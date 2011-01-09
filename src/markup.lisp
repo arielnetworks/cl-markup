4623b9883a05ceb14287bbd9743d90fa0b619533
fukamachi <e.arrows@gmail.com>
2011-01-09 13:38:14 +0900

    deleted needless code

--- src/markup.lisp
+++ src/markup.lisp
@@ -110,7 +110,6 @@
 
 (defun doctype ()
   (%write-strings
-   "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
    (case *markup-language*
      (:xml "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
      (:html "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">")
